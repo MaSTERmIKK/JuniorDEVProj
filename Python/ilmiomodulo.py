5900eb1b548023c25eff1efc7eee81f706c29ee7
Mirko Campari <82829377+MaSTERmIKK@users.noreply.github.com>
2023-05-18 16:15:12 +0200

    Add files via upload

---
+++ Python/ilmiomodulo.py
@@ -0,0 +1,26 @@
+
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
+
+def greeting(name):
+  print("Hello, " + name)
+
+passqord = 0
+
+def greetingpass(passqord):
+  print(passqord == False)
+ 
+import pickle
+from typing import Protocol
+
+obj = ""
+
+
+pickle.HIGHEST_PROTOCOL
+
+pickle.DEFAULT_PROTOCOL
+
